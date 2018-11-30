bd18108acb8174ed39fbb666b0c8ee474c2776d1
VitalyZenkou <blink812@gmail.com>
2018-11-30 21:56:13 +0300

    a

---
+++ com/course/zenkouv/src/main/java/lecture4/task2/util/Path.java
@@ -0,0 +1,14 @@
+package lecture4.task2.util;
+
+public class Path {
+
+    public static final String MESSAGE_PATH = "src\\main\\resources\\message.txt";
+    public static final String WEAPON_PATH = "src\\main\\resources\\weapon.txt";
+    public static final String ARMOR_PATH = "src\\main\\resources\\armor.txt";
+    public static final String MESSAGE_PATH_NEW_DESTINATION = "src\\main\\resources\\out\\message_Out.txt";
+    public static final String WEAPON_PATH_NEW_DESTINATION = "src\\main\\resources\\out\\weapon_Out.txt";
+    public static final String ARMOR_PATH_NEW_DESTINATION = "src\\main\\resources\\out\\armor_Out.txt";
+
+    private Path() {
+    }
+}
